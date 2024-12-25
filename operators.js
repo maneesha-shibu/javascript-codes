29969bcc3d58b58dc4ae2ce281cbd11659e98abf
maneesha-shibu <maneeshashibbu@gmail.com>
2024-12-26 00:07:54 +0530

    added operators file

--- operators.js
+++ operators.js
@@ -6,3 +6,10 @@ console.log(2**2);
  console.log(a) //-11
 
  console.log(++a) //pre incrementation - 12
+
+
+console.log(10 == 10)
+console.log(10 == '10');
+
+//strict equal to
+console.log(10 === '10')
