82df1d14f19d6d487a3b54f4b05b4f8562d2df93
maneesha-shibu <maneeshashibbu@gmail.com>
2024-12-26 00:01:25 +0530

    added operators file

---
+++ operators.js
@@ -0,0 +1,8 @@
+//arithematic operators
+console.log(10+20);
+console.log(2**2);
+ var a = 10;
+ console.log(a++) //post incrementation - 10
+ console.log(a) //-11
+
+ console.log(++a) //pre incrementation - 12
