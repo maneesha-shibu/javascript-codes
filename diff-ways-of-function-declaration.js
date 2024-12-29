b5c3576cb4e5b72b93be598df0efa3986badf40f
maneesha-shibu <maneeshashibbu@gmail.com>
2024-12-29 22:50:20 +0530

    added different ways of  functions file

---
+++ diff-ways-of-function-declaration.js
@@ -0,0 +1,19 @@
+//functiondeclaration
+function add(a,b) {
+    console.log(a+b);
+}
+add(100,200);
+
+//function expression
+const sub =function(a,b) {
+    console.log(a-b)
+}
+console.log(sub);
+
+// //arrow function
+// const mul = (a,b) => console.log(a*b);
+// mul(10,20)
+
+// const msg = () => "hello world";
+// let res = msg();
+// console.log(res);
