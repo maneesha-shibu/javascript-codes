7306bb69182e233073d1eca2eff69111dd03c192
maneesha-shibu <maneeshashibbu@gmail.com>
2024-12-29 23:28:10 +0530

    added JSON Object file

---
+++ JSONObject.js
@@ -0,0 +1,17 @@
+const s1 = {
+    Name : 'maneesha',
+    Age : 24
+}
+
+const s2 = {
+    Name : 'ankith',
+    Email : 'ankith@gmail.com'
+}
+
+const s3 = {
+    Name : 'akash',
+    Work_exp : {
+        XYZ : '3 Months',
+        ABC : '12 Months'
+    }
+}
