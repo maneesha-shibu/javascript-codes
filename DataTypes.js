017d1b71253223bd5598d3eee2f7a8aae8d97c28
maneesha-shibu <maneeshashibbu@gmail.com>
2024-12-29 23:15:26 +0530

    added data types file

---
+++ DataTypes.js
@@ -0,0 +1,19 @@
+//number data  type
+var a = 90;
+console.log(a, typeof a);
+
+// string data type
+var b = 'kodnest'
+console.log(b, typeof b);
+
+//boolean data type
+var c = true;
+console.log(c, typeof c);
+
+//undefined data type
+let d;
+console.log(d);
+
+//null data type
+let e = null;
+console.log(e);
